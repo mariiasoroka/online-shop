03bdb9998ce6a3ec6e21187e24efe2dfd21fd455
mariiasoroka <maria.soroka28@gmail.com>
2020-02-17 17:08:16 +0000

    Some new things

--- js/products-service.js
+++ js/products-service.js
@@ -5,7 +5,10 @@ class ProductsService {
     }
     async getProducts() {
         if (!this.products) {
-            this.products = await (await fetch('products.json')).json();
+            const response = await fetch('products.json');
+            const data = await response.json();
+            data.sort( (a, b) => b.price - a.price );
+            this.products = data;
         }
         return this.products;
     }
